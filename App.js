b4bf1fe8c0a61e4888fe761266e10c9148891748
Umpz <46953365+PeerawatUmp@users.noreply.github.com>
2021-12-30 22:24:53 +0700

    search API

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import SearchScreen from "./src/screens/SearchScreen";
 
-const navigator = createStackNavigator({
-  Search: SearchScreen
-},{
-  initialRouteName: 'Search',
-  defaultNavigationOptions: {
-    title: 'BussinessSearch'
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+  },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "BussinessSearch",
+    },
   }
-});
+);
 
 export default createAppContainer(navigator);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      console.log("done");
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage("Something went wrong!");
+    }
+  };
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onSubmit={() => console.log("term was submitted")}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>{term}</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
